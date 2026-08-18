69b91d5ef38bf218869458a8ad6d15f9a713d5ef
Devender Singh <devendersinghmehra07@gmail.com>
2026-08-18 16:08:19 +0000

    fix: set `transition: none` during active seek interaction
    bug:
    
    - seek thumb and progress bar styling relied on hover and
      active pseudo selectors, which worked on desktop but behaved
      inconsistently on mobile.
    - setting `transition-duration: 0s` did not change duration
      of an already-running transition.
    
    fix:
    
    - use ternary operator instead of pseudo selector to apply styles
    - set `transition: none` to immediately disable an already-running transition

--- src/components/web-player/custom-skin/ProgressBar.jsx
+++ src/components/web-player/custom-skin/ProgressBar.jsx
@@ -1,12 +1,14 @@
 import { useMedia, usePlayer } from "@videojs/react";
 import { useEffect, useRef } from "react";
 import { useThrottle } from "../../Hooks/useThrottle";
+
 // !mainBarBgClassName,progressBarClassname, ThumbClassName, make ti configuration later.
 //! i might watn the pointer capturing to get release as soon as the poitner is out of video player.
 //! there is a problem the thumb is going beyound the end of track,
 //! i might want to, do nto let the thumb get past the tracks,
 //axis handling
 /* progress bar should be in perfect sync with the actual video current duration */
+//evaluate wheather to move this to getBoudingRect or not.
 
 export function ProgressBar({ axis: setAxis }) {
   //--axis handling
@@ -29,7 +31,7 @@ export function ProgressBar({ axis: setAxis }) {
     if (thumb.current.focused) return; //skip seek if interaction is active
     // --
     seek(time).finally(() => {
-      if (!thumb.current.focused) play(); 
+      if (!thumb.current.focused) play();
       //seek is called but interaction started again, so do not play.
       //as it will disturb the expected re-render sequence
     });
@@ -46,8 +48,8 @@ export function ProgressBar({ axis: setAxis }) {
   if (duration === 0) return null;
 
   function applyStyles(value) {
-    thumbEl.current.style.left = value;
     progressBarEl.current.style.width = value;
+    thumbEl.current.style.left = value;
   }
 
   function handlePointerUp(e) {
@@ -56,7 +58,7 @@ export function ProgressBar({ axis: setAxis }) {
     // --
     const { trackLength, currentPosition } = thumb.current;
     // progress bar width in percentage
-    const ProgressPercentage = (currentPosition / trackLength) * 100; //*proportion can only work
+    const ProgressPercentage = (currentPosition / trackLength) * 100; //*proportion can also work
     //use progressPercentage to find current duration from duration
     const toSeek = Math.round(duration * (ProgressPercentage / 100));
     throttleSeek(toSeek);
@@ -101,24 +103,19 @@ export function ProgressBar({ axis: setAxis }) {
       onPointerMove={onPointerMoveHandler}
       onPointerUp={handlePointerUp}
       style={{ "--move-duration": `${duration - currentTime}s` }}
-      className={`cursor-pointer ${isAxisX ? "touch-pan-y" : "touch-pan-x"} py-3 select-none *:pointer-events-none active:cursor-grabbing active:[&>div>*]:duration-0! hover:[&>div>button]:opacity-100`}
+      className={`cursor-pointer ${isAxisX ? "touch-pan-y" : "touch-pan-x"} relative py-3 select-none *:pointer-events-none active:cursor-grabbing hover:[&>div>button]:opacity-100`}
       // active:[&>div]:h-1.75 try it later
     >
       {/* <input className="w-full rounded-none bg-amber-400" type="range" name="" id="" /> see if the progress bar can be built on top of it, it is more semantic*/}
       <div className="relative flex h-1 w-full items-center bg-gray-400">
         <span
           ref={progressBarEl}
-          className="inline-block h-full bg-white transition-[width] duration-(--move-duration) ease-linear"
+          className={`inline-block h-full bg-white duration-(--move-duration) ease-linear ${paused ? "transition-none" : "transition-[width]"}`}
         ></span>
-
+        {/* add a size up animation for thumb btn */}
         <button
           ref={thumbEl}
-          style={{
-            transitionProperty: "left, opacity",
-            transitionTimingFunction: "linear, ease-in-out",
-            transitionDuration: "var(--move-duration), 75ms",
-          }}
-          className="absolute size-4 -translate-x-1/2 rounded-full bg-white opacity-0 shadow-2xl"
+          className={`absolute size-4 -translate-x-1/2 rounded-full bg-white shadow-2xl ${paused ? "transition-none" : "opacity-0 transition-[left,opacity]"} duration-[var(--move-duration),75ms] ease-[linear,ease-in-out]`}
         ></button>
       </div>
     </div>
